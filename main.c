5252220b83974ff34b7b56f3948001f0bf393df1
Ye Feng <ye.feng@coconutislandstudio.com>
2011-03-09 18:12:43 +0800

    Compare the previous and current frame to avoid output duplicate frames, also fix frame interval issue

--- main.c
+++ main.c
@@ -3,6 +3,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+static int compare_images(cairo_surface_t* image1, cairo_surface_t* image2)
+{
+  
+  unsigned char* data1, *data2;
+  int i, j, width, height, row_stride;
+  int diff = 0;
+
+  if (!image1 || !image2 )
+  {
+    return INT_MAX;
+  }
+  if (cairo_image_surface_get_width(image1) != cairo_image_surface_get_width(image2) ||
+      cairo_image_surface_get_height(image1) != cairo_image_surface_get_height(image2))
+  {
+    return INT_MAX;
+  }
+
+  width = cairo_image_surface_get_width(image1);
+  height = cairo_image_surface_get_height(image2);
+
+  data1 = cairo_image_surface_get_data(image1);
+  data2 = cairo_image_surface_get_data(image2);
+
+  row_stride = cairo_image_surface_get_stride(image1);
+
+  for(i = 0; i < height; ++i )
+  {
+    for(j = 0; j < width*4; ++j )
+    {
+      if ( data1[i*row_stride+j] != data2[i*row_stride+j] )
+      {
+        diff++;
+      }
+    }
+  }
+  return diff;
+}
+
 static void print_usage( const char* appname)
 {
   printf("Usage: %s {-w: _width_} {-h: _height_} {-f: _frames_} [input swf] [output filename]\n", appname );
@@ -12,8 +50,7 @@ int main (int argc, char **argv)
 {
   SwfdecPlayer *player;
   SwfdecURL *url;
-  cairo_surface_t *surface;
-  cairo_t *cr;
+  
   int i;
   char output_filename_serial[256] = {0};
   unsigned long frame_interval = 0;
@@ -22,6 +59,9 @@ int main (int argc, char **argv)
   unsigned int override_height = 0;
   const char* input_filename = NULL;
   const char* output_filename = NULL;
+  int base_index = 0;
+  cairo_surface_t *surface[2] = {NULL, NULL};
+  cairo_t *cr[2] = {NULL, NULL};
 
   /* Parse the command line arguments */
   for(i = 1; i < argc; ++i )
@@ -90,20 +130,56 @@ int main (int argc, char **argv)
     swfdec_player_set_size(player, override_width, override_height);
   }
 
+  printf("Default frame rate: %f\n", swfdec_player_get_rate(player));
   frame_interval = (unsigned long)(1000.0 / swfdec_player_get_rate(player));
+  printf("Default frame interval: %ld\n", frame_interval);
+
+  //swfdec_player_advance (player, frame_interval/2);
 
   for(i = 0; i < frame_count; ++i )
   {
-    surface = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, override_width, override_height);
-    cr = cairo_create (surface);
+    int new_index = (base_index + 1) % 2;
+    surface[new_index] = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, override_width, override_height);
+    cr[new_index] = cairo_create (surface[new_index]);
     // render the image
-    swfdec_player_render (player, cr);
-    sprintf(output_filename_serial, "%s%3.3d.png", output_filename, i+1);
-    cairo_surface_write_to_png (surface, output_filename_serial);
-    swfdec_player_advance (player, frame_interval);
-    cairo_destroy (cr);
-    cairo_surface_destroy (surface);
+    swfdec_player_render (player, cr[new_index]);
+
+    
+
+#if 1
+    if ( compare_images(surface[base_index], surface[new_index]) <= 5 )
+    {
+      cairo_destroy (cr[new_index]);
+      cairo_surface_destroy (surface[new_index]);
+      cr[new_index] = NULL;
+      surface[new_index] = NULL;
+
+      frame_interval = swfdec_player_get_next_event(player);
+      if ( frame_interval > 0 )
+        swfdec_player_advance (player, frame_interval);
+    }
+    else
+#endif
+    {
+
+      cairo_destroy (cr[base_index]);
+      cairo_surface_destroy (surface[base_index]);
+      cr[base_index] = NULL;
+      surface[base_index] = NULL;
+
+      sprintf(output_filename_serial, "%s%3.3d.png", output_filename, i+1);
+      cairo_surface_write_to_png (surface[new_index], output_filename_serial);
+      frame_interval = swfdec_player_get_next_event(player);
+      if ( frame_interval > 0 )
+        swfdec_player_advance (player, frame_interval);
+      
+
+      base_index = new_index;
+    }
   }
+
+  cairo_destroy (cr[base_index]);
+  cairo_surface_destroy (surface[base_index]);
 
   g_object_unref (player);
   swfdec_url_free(url);
